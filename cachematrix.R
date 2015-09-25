22df6aed937331cc90373700b67323ba38d9f40d
Reynaldo10 <reynaldo200383@gmail.com>
2015-09-25 14:45:33 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,20 @@
 ## Put comments here that give an overall description of what your 
 ## functions do 
  
-## Write a short comment describing this function 
+ ## Write a short comment describing this function 
  
 ################################################################################ 
 ## This function creates a special "matrix" object that can cache its inverse ## 
 ################################################################################ 
  
-makeCacheMatrix <- function(x = matrix()) {## creates function with 'x' argument of matrix type 
+ makeCacheMatrix <- function(x = matrix()) {## creates function with 'x' argument of matrix type 
   i <- NULL                                ## global variable 'i' initializes NULL 
   set <- function(y){                      ## creates function for set other matrix with 'y' argument 
     x <<- y                                ## assigns the 'y' values to the 'x' global variable  
     i <<- NULL                             ## assigns value NULL to 'i' global variable   
     print(x)                               ## prints 'x' variable  
   } 
-    
+   
   get <- function() x                      ## creates function that gets the value of 'x' global variable (original matrix) 
   setsolve <- function(inver) i <<- inver  ## creates function that assigns the new inverse matrix to the 'i' global variable  
   getsolve <- function() i                 ## creates function that gets the value of 'i' global variable (inverse matrix) 
@@ -24,14 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {## creates function with 'x' argument
   } 
  
  
- 
 ## Write a short comment describing this function 
  
 ################################################################################################## 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above ## 
 ################################################################################################## 
  
-cacheSolve <- function (list,...){            ## creates function with 'list' argument of list type 
+ cacheSolve <- function (list,...){            ## creates function with 'list' argument of list type 
   inv <- list$getsolve()                      ## assigns the value of  'i' global variable to the 'inv' variable  
   if(!is.null(inv)){                          ## validates if the value of 'inv' variable it's null 
     message ("getting inverse matrix cached") ## shows message 
